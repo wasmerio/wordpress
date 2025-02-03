e359e2af2706e574a3891b081e304cda6d402292
Arshia Ghafoori <arshia001@live.com>
2025-02-03 12:14:09 +0000

    Remove `--debug` from wp-cli invocations

--- install/install.sh
+++ install/install.sh
@@ -2,10 +2,11 @@
 
 set -eo
 
+echo "Installing WP . . ."
+
 php /app/wp-cli.phar \
   --allow-root \
   --path=/app \
-  --debug \
   core install \
   --url=$WP_INSTALL_APP_DOMAIN \
   --title=$WP_INSTALL_TITLE \
@@ -14,14 +15,16 @@ php /app/wp-cli.phar \
   --admin_email=$WP_INSTALL_EMAIL \
   --locale=$WP_INSTALL_LANGUAGE
 
-php /app/wp-cli.phar \
-  --allow-root \
-  --path=/app \
-  --debug \
-  theme install /app/install/twentytwentyfive.zip
+echo "Installing default theme . . ."
 
 php /app/wp-cli.phar \
   --allow-root \
   --path=/app \
-  --debug \
+  theme install /app/install/twentytwentyfive.zip
+
+echo "Installing theme language . . ."
+
+php /app/wp-cli.phar \
+  --allow-root \
+  --path=/app \
   language theme install --all $WP_INSTALL_LANGUAGE
