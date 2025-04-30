cd3c5278976b02ef98957414797250524f251f53
Lorentz Kinde <lorentz@wasmer.io>
2025-04-30 14:55:58 +0300

    debug: Added sleep at beginning of setup script

--- install/install.sh
+++ install/install.sh
@@ -6,6 +6,9 @@ set -e
 # doesn't work because we don't have the stty command it uses.
 export COLUMNS=80
 
+echo "Sleeping for 30 seconds"
+sleep 30
+
 echo "Creating required directories..."
 
 mkdir -p /app/wp-content/plugins
